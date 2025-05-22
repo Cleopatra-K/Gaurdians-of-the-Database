0af71ea9bb952ec2f392d465a559b1d856179596
cleoluvs2code <cleopatrakwenda09@gmail.com>
2025-05-22 17:33:21 +0200

    Update index.php

--- Frontend/php/index.php
+++ Frontend/php/index.php
@@ -8,7 +8,7 @@ include 'header.php';
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Parcellas Products</title>
+        <title>WheeelDeal Products</title>
 
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="../css/productsstyle.css">
