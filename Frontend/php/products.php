aec1d98d353d2c49a4b1ae1597689d76f1d948f4
Neo <u23002167@tuks.co.za>
2025-05-24 18:18:41 +0200

    commented out products filter to user the filters.php and include filters, which will also include config correctly

--- Frontend/php/products.php
+++ Frontend/php/products.php
@@ -1,6 +1,7 @@
 <?php
 // Neo (NMC) Machaba, u23002167
-include 'config.php';
+// include 'config.php';
+include 'filters.php';
 
 // Initialize variables
 $products = [];
@@ -39,57 +40,57 @@ try {
 } catch (Exception $e) {
     $error = $e->getMessage();
 }
-function getFilteredSortedProducts($filters = [], $sortBy = "") {
-    $where = [];
-    $params = [];
-    if (!empty($filters['brand'])) {
-        $where[] = "brand = ?";
-        $params[] = $filters['brand'];
-    }
-    if (!empty($filters['category'])) {
-        $where[] = "categories LIKE ?";
-        $params[] = '%' . $filters['category'] . '%';
-    }
-    if (!empty($filters['distributor'])) {
-        $where[] = "distributor = ?";
-        $params[] = $filters['distributor'];
-    }
+// function getFilteredSortedProducts($filters = [], $sortBy = "") {
+//     $where = [];
+//     $params = [];
+//     if (!empty($filters['brand'])) {
+//         $where[] = "brand = ?";
+//         $params[] = $filters['brand'];
+//     }
+//     if (!empty($filters['category'])) {
+//         $where[] = "categories LIKE ?";
+//         $params[] = '%' . $filters['category'] . '%';
+//     }
+//     if (!empty($filters['distributor'])) {
+//         $where[] = "distributor = ?";
+//         $params[] = $filters['distributor'];
+//     }
 
-    if (isset($filters['availability'])) {
-        $where[] = "availability = ?";
-        $params[] = $filters['availability'] ? 1 : 0;
-    }
+//     if (isset($filters['availability'])) {
+//         $where[] = "availability = ?";
+//         $params[] = $filters['availability'] ? 1 : 0;
+//     }
 
-    $sql = "SELECT * FROM products";
-    if ($where) {
-        $sql .= " WHERE " . implode(" AND ", $where);
-    }
-    if ($sortBy === "title-desc") {
-        $sql .= " ORDER BY title DESC";
-    } elseif ($sortBy === "brand") {
-        $sql .= " ORDER BY brand ASC";
-    } elseif ($sortBy === "category") {
-        $sql .= " ORDER BY categories ASC";
-    } elseif ($sortBy === "distributor") {
-        $sql .= " ORDER BY distributor ASC";
-    } elseif ($sortBy === "availability") {
-        $sql .= " ORDER BY availability DESC";
-    }
+//     $sql = "SELECT * FROM products";
+//     if ($where) {
+//         $sql .= " WHERE " . implode(" AND ", $where);
+//     }
+//     if ($sortBy === "title-desc") {
+//         $sql .= " ORDER BY title DESC";
+//     } elseif ($sortBy === "brand") {
+//         $sql .= " ORDER BY brand ASC";
+//     } elseif ($sortBy === "category") {
+//         $sql .= " ORDER BY categories ASC";
+//     } elseif ($sortBy === "distributor") {
+//         $sql .= " ORDER BY distributor ASC";
+//     } elseif ($sortBy === "availability") {
+//         $sql .= " ORDER BY availability DESC";
+//     }
 
-    $stmt = $conn->prepare($sql);
-    if ($params) {
-        $types = str_repeat('s', count($params));
-        $stmt->bind_param($types, ...$params);
-    }
-    $stmt->execute();
-    $result = $stmt->get_result();
+//     $stmt = $conn->prepare($sql);
+//     if ($params) {
+//         $types = str_repeat('s', count($params));
+//         $stmt->bind_param($types, ...$params);
+//     }
+//     $stmt->execute();
+//     $result = $stmt->get_result();
 
-    $products = [];
-    while ($row = $result->fetch_assoc()) {
-        $products[] = $row;
-    }
-    return $products;
-}
+//     $products = [];
+//     while ($row = $result->fetch_assoc()) {
+//         $products[] = $row;
+//     }
+//     return $products;
+// }
 
 $products = getFilteredSortedProducts($filters, "title-desc");
     
